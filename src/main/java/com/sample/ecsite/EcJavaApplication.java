369873e0d33b666b09a7be35b9c3ca6e5092ea7c
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-03-29 12:52:20 +0900

    1st commit

---
+++ src/main/java/com/sample/ecsite/EcJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sample.ecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/ecsite/EcJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.ecsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
